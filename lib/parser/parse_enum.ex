4460d4bf01d2e7f4672c8e4b91942d04f9922a62
Tadas Verbyla <tadas.verbyla@gmail.com>
2023-10-25 11:47:52 +0300

    Expanded parse_map
    Finished the return values of parse_map and added UUID functionality for unique identification of outputs

--- lib/parser/parse_enum.ex
+++ lib/parser/parse_enum.ex
@@ -1,12 +1,17 @@
 defmodule ElixirToAstGenerator.Parser.ParseEnum do
   def parse_map(input_name) do
     constant = input_name |> ElixirToAstGenerator.StringUtils.SnakeToCamel.snake_to_camel
-    [constants: [constant],
+    #output_name = ["output-", UUID.uuid4]
+    output_name = "output"
+    [extends: ["Integers", "Sequences"],
+    constants: [constant],
+    variables: [input_name, output_name],
     type_ok: [],
     init: [ "/\\ #{input_name} = #{constant}",
-            "/\\ output = <<>>"],
+            "/\\ #{output_name} = <<>>"],
     next: [ "/\\ #{input_name} # <<>>",
-            "/\\ output' = Append(output, Head(#{input_name}) * 2)",
-            "/\\ #{input_name}' = Tail(#{input_name})]]"]]
+            "/\\ #{[output_name, "'"]} = Append(#{output_name}, Head(#{input_name}) * 2)",
+            "/\\ #{input_name}' = Tail(#{input_name})]]"],
+    stutter: "<<#{input_name}, #{output_name}>>"]
   end
 end
