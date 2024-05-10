08ac9283392ac9227e9085f939750ed63c1779b5
Tadas Verbyla <90239993+TadasVerbyla@users.noreply.github.com>
2024-05-10 14:50:08 +0300

    Removed AST simplification
    Changed the generator to process Elixir AST without simplifying it.

--- lib/parser/ast_parser.ex
+++ lib/parser/ast_parser.ex
@@ -1,77 +1,93 @@
 defmodule ElixirToTlaGenerator.Parser.AstParser do
 
-  def parse_ast(simplified_ast) do
-    [_tlagen_name, clauses] = simplified_ast
+  def parse_ast(ast) do
+    [_tlagen_name, clauses] = ast
 
     headers = get_headers(clauses)
     all_parameters = get_all_parameters(headers)
     guards = get_all_guards(headers)
     clause_names = get_all_clause_names(headers)
-    unique_paramteres = Enum.uniq(all_parameters)
+    f = fn({atom, _, nil}, acc) ->
+      case Enum.member?(acc, {atom, [], nil}) do
+        true ->
+          acc
+        false ->
+          acc ++ [{atom, [], nil}]
+      end
+    end
+    unique_paramteres = Enum.flat_map(all_parameters, & &1) |>
+    Enum.reduce([], f)
     full_guards = add_guard_negation(guards)
     unique_clause_names = Enum.uniq(clause_names)
-
     clause_expressions = get_expressions(clauses)
     flattened_clause_expressions = Enum.map(clause_expressions, fn clause ->
-      flatten_expression_wrapper(Enum.at(clause, 0))
+      flatten_expression_wrapper(clause)
     end)
     function_groups = Enum.map(flattened_clause_expressions, fn flattened_clause ->
       group_flattened_wrapper(flattened_clause, unique_clause_names)
     end)
+
+    # TODO: FIX NUMBERING OF GROUPED ELEMENTS
     [unique_paramteres, full_guards, function_groups]
   end
 
-  def get_headers([[header, _body] | []]) do
+  #----------------------------------------------------------------------------
+
+  # Handles headers with multiple conds
+  def get_headers([{:defp, _, [{:when, _, header}, _do_block]} | []]) do
     header
   end
 
-  def get_headers([[header, _body] | rest]) do
-    [header | get_headers(rest)]
+  def get_headers([{:defp, _, [{:when, _, header}, _do_block]} | rest]) do
+    [header, get_headers(rest)]
   end
 
+  # Handles headers with single cond
+  def get_headers([{:defp, _, [header, _do_block]} | []]) do
+    [header]
+  end
 
-  def get_all_parameters([_name, [parameters, _guards]]) do
+  def get_headers([{:defp, _, [header, _do_block]} | rest]) do
+    [[header], get_headers(rest)]
+  end
+
+  #----------------------------------------------------------------------------
+
+  # Gets clause parameters (variables) from headers
+  def get_all_parameters([[{_name, _, parameters} | _guards]]) do
     [parameters]
   end
 
-  def get_all_parameters([_name, parameters]) do
-    [parameters]
-  end
 
-  def get_all_parameters([[_name, [parameters, _guards]] | rest]) do
+  def get_all_parameters([[{_name, _, parameters} | _guards] | rest]) do
     [parameters | get_all_parameters(rest)]
   end
 
-  def get_all_parameters([[_name, parameters] | rest]) do
-    [parameters | get_all_parameters(rest)]
-  end
+  #----------------------------------------------------------------------------
 
-
-  def get_all_guards([_name, [_parameters, guards]]) do
+  # Gets clause guards from headers
+  def get_all_guards([[_parameters | guards]]) do
     [guards]
   end
 
-  def get_all_guards([_name, _parameters]) do
-    [[]]
-  end
 
-  def get_all_guards([[_name, [_parameters, guards]] | rest]) do
+  def get_all_guards([[_parameters | guards] | rest]) do
     [guards | get_all_guards(rest)]
   end
 
-  def get_all_guards([[_name, _parameters] | rest]) do
-    [[] | get_all_guards(rest)]
-  end
+  #----------------------------------------------------------------------------
 
-
-  def get_all_clause_names([name, _body]) do
+  # Gets names of each function clause
+  def get_all_clause_names([[{name, _, _parameters} | _guards]]) do
     [name]
   end
 
-  def get_all_clause_names([[name, _body] | rest]) do
+
+  def get_all_clause_names([[{name, _, _parameters} | _guards] | rest]) do
     [name | get_all_clause_names(rest)]
   end
 
+  #----------------------------------------------------------------------------
 
   def add_guard_negation(guards) do
     Enum.map(guards, fn(guard) ->
@@ -79,38 +95,19 @@ defmodule ElixirToTlaGenerator.Parser.AstParser do
     end)
   end
 
+  #----------------------------------------------------------------------------
 
-  def get_expressions([[_header, body] | []]) do
+  def get_expressions([{_def, _, [_header, [do: body]]} | []]) do
     [body]
   end
 
-  def get_expressions([[_header, body] | rest]) do
+  def get_expressions([{_def, _, [_header, [do: body]]} | rest]) do
     [body | get_expressions(rest)]
   end
 
+  #----------------------------------------------------------------------------
 
-
-
-  @spec flatten_expression(number() | binary(), number(), list(any())) :: {number() | atom() | binary(), number(), list(any())}
-  def flatten_expression(constant, fn_counter, functions) when is_number(constant) or is_binary(constant) do
-    {constant, fn_counter, functions}
-  end
-
-  @spec flatten_expression({atom(), any()}, number(), list(any())) :: {list(atom()), number(), list(any())}
-  def flatten_expression([atom, nil], fn_counter, functions) do
-    {[atom, nil], fn_counter, functions}
-  end
-
-  def flatten_expression(constant, 0, functions) when is_number(constant) or is_binary(constant) do
-    {constant, 0, functions ++ [{0, constant}]}
-  end
-
-  def flatten_expression([atom, nil], 0, functions) do
-    {[atom, nil], 0, functions ++ [{0, [atom, nil]}]}
-  end
-
-  @spec flatten_expression({atom(), list(any())}, number(), list(any())) :: {list(any()), number(), list(any())}
-  def flatten_expression([atom, parameters], fn_counter, functions) do
+  def flatten_expression({atom, _, parameters}, fn_counter, functions) when is_list(parameters) do
 
     f = fn(parameter, {acc_parameters, acc_counter, acc_functions}) ->
       {flattened_parameter, new_fn_counter, new_functions} = flatten_expression(parameter, acc_counter, acc_functions)
@@ -129,7 +126,23 @@ defmodule ElixirToTlaGenerator.Parser.AstParser do
     {function_id, new_counter + 1, new_functions ++ [function]}
   end
 
-  @spec flatten_expression_wrapper(list(any())) :: list(any())
+  def flatten_expression(constant, fn_counter, functions) when is_number(constant) or is_binary(constant) do
+    {constant, fn_counter, functions}
+  end
+  def flatten_expression(constant, 0, functions) when is_number(constant) or is_binary(constant) do
+    {constant, 0, functions ++ [{0, constant}]}
+  end
+
+  def flatten_expression(constant = {_atom, _, _}, fn_counter, functions) do
+    {constant, fn_counter, functions}
+  end
+  def flatten_expression(constant = {_atom, _, _}, 0, functions) do
+    {constant, 0, functions ++ [{0, constant}]}
+  end
+
+  #----------------------------------------------------------------------------
+
+  #@spec flatten_expression_wrapper(list(any())) :: list(any())
   def flatten_expression_wrapper(expressions) do
     {last_func, _, result} = flatten_expression(expressions, 0, [])
     case length(result) == 0 do
@@ -139,6 +152,8 @@ defmodule ElixirToTlaGenerator.Parser.AstParser do
         result
     end
   end
+
+  #----------------------------------------------------------------------------
 
   def group_flattened([], _recursive_operators, _groups, _current_group) do
     []
--- test/flatten_test.exs
+++ test/flatten_test.exs
@@ -1,65 +1,57 @@
 defmodule FlattenTest do
   use ExUnit.Case
-  doctest ElixirToTlaGenerator.Parser.AstParser
-
-
+  doctest ElixirToTlaGenerator.Parser.AstParserUnsimplified
 
   test "flatten_constant_integer" do
-    assert ElixirToTlaGenerator.Parser.AstParser.flatten_expression_wrapper(
-      # 1
+    assert ElixirToTlaGenerator.Parser.AstParserUnsimplified.flatten_expression_wrapper(
       (quote do: 1)
     ) == [{0, 1}]
   end
 
   test "flatten_constant_string" do
-    assert ElixirToTlaGenerator.Parser.AstParser.flatten_expression_wrapper(
-      # string
+    assert ElixirToTlaGenerator.Parser.AstParserUnsimplified.flatten_expression_wrapper(
       (quote do: "string")
     ) == [{0, "string"}]
   end
 
   test "flatten_variable" do
-    assert ElixirToTlaGenerator.Parser.AstParser.flatten_expression_wrapper(
-      # n
-      [:n, nil]
-    ) == [{0, [:n, nil]}]
+    assert ElixirToTlaGenerator.Parser.AstParserUnsimplified.flatten_expression_wrapper(
+      (quote do: n)
+    ) == [{0, {:n, [], FlattenTest}}]
   end
 
   test "flatten_single_expression" do
-    assert ElixirToTlaGenerator.Parser.AstParser.flatten_expression(
-      # n - 1
-      [:-, [[:n, nil], 1]],
+    assert ElixirToTlaGenerator.Parser.AstParserUnsimplified.flatten_expression(
+      (quote do: n - 1),
       0,
       []
     ) == {{:fn_nr, 0}, 1, [
-                            {0, [:-, [[:n, nil], 1]]}
+                            {0, [:-, [{:n, [], FlattenTest}, 1]]}
                           ]}
     # Example of how return value for function is handled internally
     # First element is the last processed function id tag
   end
 
   test "flatten_double_expression" do
-    assert ElixirToTlaGenerator.Parser.AstParser.flatten_expression_wrapper(
-      # fib(n - 1)
-      [:fib, [[:-, [[:n, nil], 1]]]]
+    assert ElixirToTlaGenerator.Parser.AstParserUnsimplified.flatten_expression_wrapper(
+      (quote do: fib(n - 1))
     ) ==  [
-            {0, [:-, [[:n, nil], 1]]},
-            {1, [:fib, [{:fn_nr, 0}]]}
+            {0, [:-, [{:n, [], FlattenTest}, 1]]},
+            {1, [:fib, [fn_nr: 0]]}
           ]
     # v0 = n - 1
     # v1 = fib [v0]
   end
 
   test "flatten_full" do
-    assert ElixirToTlaGenerator.Parser.AstParser.flatten_expression_wrapper(
-      # fib(n - 1) + fib(n - 2)
-      [:+, [[:fib, [[:-, [[:n, nil], 1]]]], [:fib, [[:-, [[:n, nil], 2]]]]]]
+    assert ElixirToTlaGenerator.Parser.AstParserUnsimplified.flatten_expression_wrapper(
+      (quote do: fib(n - 1) + fib(n - 2))
     ) ==  [
-            {0, [:-, [[:n, nil], 1]]},
-            {1, [:fib, [{:fn_nr, 0}]]},
-            {2, [:-, [[:n, nil], 2]]},
-            {3, [:fib, [{:fn_nr, 2}]]},
-            {4, [:+, [{:fn_nr, 1}, {:fn_nr, 3}]]}
+            {0, [:-, [{:n, [], FlattenTest}, 1]]},
+            {1, [:fib, [fn_nr: 0]]},
+            {2, [:-, [{:n, [], FlattenTest}, 2]]},
+            {3, [:fib, [fn_nr: 2]]},
+            {4, [:+, [fn_nr: 1, fn_nr: 3]]}
           ]
     # v1 = n - 1
     # v2 = fib(v1)
